476369cc46661f307629414cd12fbe07eea4a93a
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-20 14:19:26 -0800

    create purchaseData

---
+++ cart/data.js
@@ -0,0 +1,22 @@
+export purchaseData = [
+    {
+        id: 'book1',
+        quantity: 3,
+    },
+    {
+        id: 'book2',
+        quantity: 2,
+    },
+    {
+        id: 'book3',
+        quantity: 1,
+    },
+    {
+        id: 'book4',
+        quantity: 5,
+    },
+    {
+        id: 'book4',
+        quantity: 4,
+    },
+]
