3833e073847125aa13f1091ea382726865fc311d
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-20 08:19:04 -0800

    fix photos

--- products/books.js
+++ products/books.js
@@ -3,7 +3,7 @@ export const books = [
     {
         id: 'book1',
         title: 'We Begin At The End',
-        image: '/assets/book1.png',
+        image: 'book1.png',
         author: 'Chris Whitaker',
         category: 'Fiction',
         price: 19.95,
@@ -11,7 +11,7 @@ export const books = [
     {
         id: 'book2',
         title: 'Building for Better Living',
-        image: '/assets/book2.png',
+        image: 'book2.png',
         author: 'A. Quincy Jones',
         category: 'Non-Fiction',
         price: 29.95,
@@ -19,7 +19,7 @@ export const books = [
     {
         id: 'book3',
         title: 'Fire On The Mountain',
-        image: '/assets/book3.png',
+        image: 'book3.png',
         author: 'Edward Abbey',
         category: 'Fiction',
         price: 14.95,
@@ -27,7 +27,7 @@ export const books = [
     {
         id: 'book4',
         title: 'The Signal',
-        image: '/assets/book4.png',
+        image: 'book4.png',
         author: 'Ron Carlson',
         category: 'Fiction',
         price: 14.95,
@@ -35,9 +35,10 @@ export const books = [
     {
         id: 'book5',
         title: 'Neutra',
-        image: '/assets/book5.png',
+        image: 'book5.png',
         author: 'Barbara Lamprecht',
         category: 'Non-Fiction',
         price: 19.95,
     },
+
 ];
